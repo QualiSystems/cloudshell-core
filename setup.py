23ab4e569f584a711dfa299d246e4c7cdabca0e5
olgae <olga.e@qualisystems.com>
2016-02-17 17:32:27 +0200

    fix package name

--- setup.py
+++ setup.py
@@ -11,15 +11,15 @@ with open('test_requirements.txt') as f_tests:
     required_for_tests = f_tests.read().splitlines()
 
 setup(
-    name='cloudshell-networking',
-    url='https://github.com/QualiSystems/cloudshell-networking',
+    name='cloudshell-core',
+    url='https://github.com/QualiSystems/cloudshell-core',
     author='QualiSystems',
     author_email='info@qualisystems.com',
-    packages=['cloudshell-networking'],
+    packages=['cloudshell-core'],
     install_requires=required,
     test_suite='tests',
     tests_require=required_for_tests,
     version=version_from_file,
-    description='QualiSystems Python package',
+    description='CloudShell Core Python package',
     include_package_data = True
 )
