4d2ea371928eedf2825cb1881a938460e046e89b
Roni Dover <roni.dover@gmail.com>
2016-04-03 11:08:54 +0300

    updating setup.py

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with open('test_requirements.txt') as f_tests:
 
 setup(
     name='cloudshell-core',
-    url='http://www.quali.com/',
+    url='https://github.com/QualiSystems/cloudshell-core',
     author='Quali',
     author_email='info@quali.com',
     packages=find_packages(),
@@ -20,12 +20,13 @@ setup(
     tests_require=required_for_tests,
     license='Apache 2.0',
     version=version_from_file,
-    description='Core package for CloudShell Python orchestration and automation. This package can be referenced by '
-                'code running under CloudShell to get access to logging, basic interfaces and other utilities',
+    description='Core package for CloudShell Python orchestration and automation. This package contains common'
+                'code for CloudShell packages, including logging, basic interfaces and other utilities',
     include_package_data=True,
     keywords="sandbox cloud cmp cloudshell",
     classifiers=[
         "Development Status :: 5 - Production/Stable",
+        "Programming Language :: Python :: 2.7",
         "Topic :: Software Development :: Libraries",
         "License :: OSI Approved :: Apache Software License",
     ]
