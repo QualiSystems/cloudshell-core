379c37ddef34e643bae23c1fff0cc0ff7a72defb
Alex <alexey.b@qualisystems.com>
2016-07-19 17:46:40 +0300

    Remove test numeration and test run order dependence

--- tests/core/logger/test_qs_logger.py
+++ tests/core/logger/test_qs_logger.py
@@ -31,6 +31,7 @@ class TestQSLogger(TestCase):
 
     def setUp(self):
         """ Remove all existing test Logs folders before each suite """
+        self.get_settings = qs_logger.get_settings
         self.qs_conf = os.getenv("QS_CONFIG")
         self.log_path = os.getenv("LOG_PATH")
 
@@ -56,7 +57,9 @@ class TestQSLogger(TestCase):
             for handler in logger.handlers:
                 handler.close()
 
-    def test_01_get_settings(self):
+        qs_logger.get_settings = self.get_settings
+
+    def test_get_settings(self):
         """ Test suite for get_settings method """
         exp_response = {'LOG_PATH': '../../Logs',
                         'TIME_FORMAT': '%d-%b-%Y--%H-%M-%S',
@@ -65,26 +68,26 @@ class TestQSLogger(TestCase):
 
         self.assertEqual(qs_logger.get_settings(), exp_response)
 
-    def test_02_get_accessible_log_path_default_params(self):
+    def test_get_accessible_log_path_default_params(self):
         """ Test suite for get_accessible_log_path method """
 
         path = qs_logger.get_accessible_log_path()
         self.assertTrue(re.search(r"Logs[\\/]Autoload[\\/]default--\d{2}-\w+-\d{4}--\d{2}-\d{2}-\d{2}\.log", path))
         self.assertTrue(os.path.dirname(path))
 
-    def test_03_get_accessible_log_path_path_creation(self):
+    def test_get_accessible_log_path_path_creation(self):
         """ Test suite for get_accessible_log_path method """
 
         path = qs_logger.get_accessible_log_path()
         self.assertTrue(os.path.dirname(path))
 
-    def test_04_get_accessible_log_path(self):
+    def test_get_accessible_log_path(self):
         """ Test suite for get_accessible_log_path method """
 
         path = qs_logger.get_accessible_log_path(qs_logger.get_accessible_log_path("reservation_id", "handler_name"))
         self.assertTrue(re.search(r"Logs[\\/]reservation_id[\\/]handler_name--\d{2}-\w+-\d{4}--\d{2}-\d{2}-\d{2}\.log", path))
 
-    def test_05_get_accessible_log_path_log_path_setting_missing(self):
+    def test_get_accessible_log_path_log_path_setting_missing(self):
         """ Test suite for get_accessible_log_path method """
 
         if "LOG_PATH" in os.environ:
@@ -92,19 +95,19 @@ class TestQSLogger(TestCase):
         qs_logger.get_settings = cut_settings
         self.assertIsNone(qs_logger.get_accessible_log_path())
 
-    def test_06_get_qs_logger_full_settings_default_params(self):
+    def test_get_qs_logger_full_settings_default_params(self):
         """ Test suite for get_qs_logger method """
 
         qs_logger.get_settings = full_settings
         self.assertTrue(isinstance(qs_logger.get_qs_logger().handlers[0], MultiProcessingLog))
 
-    def test_07_get_qs_logger_full_settings(self):
+    def test_get_qs_logger_full_settings(self):
         """ Test suite for get_qs_logger method """
 
         qs_logger.get_settings = full_settings
         self.assertTrue(isinstance(qs_logger.get_qs_logger(log_group='test1').handlers[0], MultiProcessingLog))
 
-    def test_08_get_qs_logger_stream_handler(self):
+    def test_get_qs_logger_stream_handler(self):
         """ Test suite for get_qs_logger method """
 
         if "LOG_PATH" in os.environ:
@@ -112,7 +115,7 @@ class TestQSLogger(TestCase):
         qs_logger.get_settings = cut_settings
         self.assertTrue(isinstance(qs_logger.get_qs_logger(log_group='test2').handlers[0], logging.StreamHandler))
 
-    def test_10_get_qs_logger_container_filling(self):
+    def test_get_qs_logger_container_filling(self):
         """ Test suite for get_qs_logger method """
 
         qs_logger.get_settings = full_settings
@@ -126,24 +129,24 @@ class TestQSLogger(TestCase):
 
         self.assertEqual(sorted(qs_logger._LOGGER_CONTAINER.keys()), sorted(["Ungrouped", "test1", "test2"]))
 
-    def test_11_normalize_buffer_decolorize(self):
+    def test_normalize_buffer_decolorize(self):
         """ Test suite for normalize_buffer method """
         self.assertEqual(qs_logger.normalize_buffer("\033[1;32;40mGreenOnWhiteBack "
                                                     "\033[4;31mRedUnderscore "
                                                     "\033[93mYellow"), "GreenOnWhiteBack RedUnderscore Yellow")
 
-    def test_12_normalize_buffer_remove_hex_symbols(self):
+    def test_normalize_buffer_remove_hex_symbols(self):
         """ Test suite for normalize_buffer method """
         self.assertEqual(qs_logger.normalize_buffer("\x00-\x08\x0b\x0c\x0e-\x1f\x7f-\xff"), "---")
 
-    def test_13_normalize_buffer_carriage_return_replacing(self):
+    def test_normalize_buffer_carriage_return_replacing(self):
         """ Test suite for normalize_buffer method """
         self.assertEqual(qs_logger.normalize_buffer("\r\n \n\r"), "\n \n\r")
 
-    def test_14_normalize_buffer_converts_tuple_to_string(self):
+    def test_normalize_buffer_converts_tuple_to_string(self):
         """ Test suite for normalize_buffer method """
         self.assertEqual(qs_logger.normalize_buffer(("test", "tuple")), "('test', 'tuple')")
 
-    def test_15_normalize_buffer_converts_dict_to_string(self):
+    def test_normalize_buffer_converts_dict_to_string(self):
         """ Test suite for normalize_buffer method """
         self.assertEqual(qs_logger.normalize_buffer({"test": "dict"}), "{'test': 'dict'}")
