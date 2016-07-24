a0915f9343af616130e9571820d6315e9615cd1c
Boris Modylevsky <borismod@gmail.com>
2016-07-24 11:01:37 +0300

    remove context from ctor of ErrorHandlingContext

--- tests/core/context/test_error_handling_context.py
+++ tests/core/context/test_error_handling_context.py
@@ -8,11 +8,10 @@ from cloudshell.core.context.error_handling_context import ErrorHandlingContext
 class TestLoggingSession(TestCase):
 
     def test_log_written_when_exception_occurs(self):
-        context = Mock()
         logger = Mock()
         logger.error = MagicMock()
         try:
-            with ErrorHandlingContext(context=context, logger=logger):
+            with ErrorHandlingContext(logger=logger):
                 raise ValueError('some value error occurred')
         except ValueError:
             self.assertTrue(logger.error.called)
