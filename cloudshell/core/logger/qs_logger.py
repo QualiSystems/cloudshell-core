fa63f1fbf2a40f9aa5b433e25918832a825e62e1
CoYe <wiseman.kostya@gmail.com>
2017-06-20 11:08:55 +0300

    log path fixes

--- cloudshell/core/logger/qs_logger.py
+++ cloudshell/core/logger/qs_logger.py
@@ -26,10 +26,10 @@ DEFAULT_TIME_FORMAT = '%Y%m%d%H%M%S'
 DEFAULT_LEVEL = 'DEBUG'
 # DEFAULT_LOG_PATH = os.path.join(os.path.dirname(os.path.abspath(__file__)), '../../', 'Logs')
 LOG_SECTION = 'Logging'
+WINDOWS_OS_FAMILY = "nt"
 
 _LOGGER_CONTAINER = {}
 _LOGGER_LOCK = threading.Lock()
-
 
 
 def get_settings():
@@ -42,9 +42,14 @@ def get_settings():
     log_format = QSConfigParser.get_setting(LOG_SECTION, 'LOG_FORMAT') or DEFAULT_FORMAT
     config['FORMAT'] = log_format
 
-    # log_path
-    log_path = QSConfigParser.get_setting(LOG_SECTION, 'LOG_PATH')
-    config['LOG_PATH'] = log_path
+    # UNIX log path
+    config['UNIX_LOG_PATH'] = QSConfigParser.get_setting(LOG_SECTION, 'UNIX_LOG_PATH')
+
+    # Windows log path
+    config['WINDOWS_LOG_PATH'] = QSConfigParser.get_setting(LOG_SECTION, 'WINDOWS_LOG_PATH')
+
+    # Default log path for all systems
+    config['DEFAULT_LOG_PATH'] = QSConfigParser.get_setting(LOG_SECTION, 'DEFAULT_LOG_PATH')
 
     # Time format
     time_format = QSConfigParser.get_setting(LOG_SECTION, 'TIME_FORMAT') or DEFAULT_TIME_FORMAT
@@ -55,41 +60,71 @@ def get_settings():
 
 # return accessable log path or None
 def get_accessible_log_path(reservation_id='Autoload', handler='default'):
-    accessible_log_path = None
     config = get_settings()
-
-    if 'LOG_PATH' in os.environ:
-        log_path = os.environ['LOG_PATH']
-    elif config['LOG_PATH']:
-        log_path = config['LOG_PATH']
-    else:
-        return None
-
-    curent_path = os.path.dirname(__file__)
-
-    if log_path.startswith('..'):
-        log_path = os.path.join(curent_path, log_path)
-
     time_format = config['TIME_FORMAT'] or DEFAULT_TIME_FORMAT
-
     log_file_name = '{0}--{1}.log'.format(handler, datetime.now().strftime(time_format))
-    # log_file_name = '{0}.log'.format(handler)
-    log_path = os.path.join(log_path, reservation_id)
+
+    log_path = _get_log_path_config(config)
+
+    if log_path:
+        env_folder = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", "..", "..", "..", "..")
+        shell_name = os.path.basename(os.path.abspath(env_folder))
+        log_path = os.path.join(log_path, reservation_id, shell_name)
+        path = _prepare_log_path(log_path=log_path,
+                                 log_file_name=log_file_name)
+        if path:
+            return path
+
+    default_log_path = config.get('DEFAULT_LOG_PATH')
+
+    if default_log_path:
+        default_log_path = os.path.join(default_log_path, reservation_id)
+        return _prepare_log_path(log_path=default_log_path,
+                                 log_file_name=log_file_name)
+
+
+def _get_log_path_config(config):
+    """Get log path based on the environment variable or Windows/Unix config setting
+
+    :param dict[str] config:
+    :rtype: str
+    """
+    if 'LOG_PATH' in os.environ:
+        return os.environ['LOG_PATH']
+
+    if os.name == WINDOWS_OS_FAMILY:
+        tpl = config.get('WINDOWS_LOG_PATH')
+        if tpl:
+            try:
+                return tpl.format(**os.environ)
+            except KeyError:
+                print "Environment variable is not defined in the template {}".format(tpl)
+    else:
+        return config.get('UNIX_LOG_PATH')
+
+
+def _prepare_log_path(log_path, log_file_name):
+    """Create logs directory if needed and return full path to the log file
+
+    :param str log_path:
+    :param str log_file_name:
+    :rtype: str
+    """
+    if log_path.startswith('..'):
+        log_path = os.path.join(os.path.dirname(__file__), log_path)
 
     log_file = os.path.join(log_path, log_file_name)
     # print(log_file)
 
     if os.path.isdir(log_path):
         if os.access(log_path, os.W_OK):
-            accessible_log_path = log_file
+            return log_file
     else:
         try:
             os.makedirs(log_path)
-            accessible_log_path = log_file
+            return log_file
         except:
             pass
-
-    return accessible_log_path
 
 
 def log_execution_info(logger_hdlr, exec_info):
@@ -103,8 +138,7 @@ def log_execution_info(logger_hdlr, exec_info):
         logger_hdlr.info('-----------------------------------------------------------\n')
 
 
-def get_qs_logger(log_group='Ungrouped', log_category ='QS', log_file_prefix='QS'):
-
+def get_qs_logger(log_group='Ungrouped', log_category='QS', log_file_prefix='QS'):
     """
     :param log_group: This folder will be grouped under this name. The default implementation of the group is a folder
     under the logs directory. According to the CloudShell logging standard pass the reservation id as this value when
@@ -168,7 +202,7 @@ def _create_logger(log_group, log_category, log_file_prefix):
     if log_path:
         # print("Logger log path: %s" % log_path)
         hdlr = MultiProcessingLog(log_path, mode='a')
-        #print 'Logger File Handler is: {0}'.format(hdlr.baseFilename)
+        # print 'Logger File Handler is: {0}'.format(hdlr.baseFilename)
     else:
         hdlr = StreamHandler(sys.stdout)
 
